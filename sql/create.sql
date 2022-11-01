fc48f58e80208cd1cfcecd1954dda89ed541bc47
ducphu12 <ducphu2400@gmail.com>
2022-11-01 11:37:23 +0700

    first commit

--- sql/create.sql
+++ sql/create.sql
@@ -1,7 +1,9 @@
 use demo_database;
 create table product(
    id int not null primary key auto_increment,
-   name varchar(100),
-   price int not null ,
+  nation varchar(100),
+ area varchar(100),
+ people int not null ,
+ GDP int not null ,
    description varchar(250)
 )
